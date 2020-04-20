4524a8595f9ac59ad4954970da05399ed30bd5e0
Justin Peel <justinpeel25@gmail.com>
2020-04-20 14:30:16 +0200

    Initial Commit

---
+++ Rock_Paper_Scissors/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rock_Paper_Scissors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rock_Paper_Scissors")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("997dd31c-1afe-46ab-94de-889d416f717a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Rock_Paper_Scissors/UnitTest1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using NUnit;
+using NUnit.Framework;
+
+namespace Rock_Paper_Scissors
+{
+    [TestFixture]
+    public class Rock_PaperScissors_Tests
+    {
+        [Test]
+        public void Play_GivenPlayerHasPaper_AndOpponentHasRock_ShouldReturnPlayerWins()
+        {
+            //Arrange
+            var userChoice = Choices.Paper;
+            var aiChoice = Choices.Rock;
+
+            //act
+            var actual = Play(userChoice, aiChoice);
+            var expected = "Wins";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+        [Test]
+        public void Play_GivenPlayerHasPaper_AndOpponentHasScissors_ShouldReturnPlayerLoses()
+        {
+            //Arrange
+            var userChoice = Choices.Paper;
+            var aiChoice = Choices.Scissors;
+
+            //act
+            var actual = Play(userChoice, aiChoice);
+            var expected = "Loss";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Play_GivenPlayerHasPaper_AndOpponentHasPaper_ShouldReturnDraw()
+        {
+            //Arrange
+            var userChoice = Choices.Paper;
+            var aiChoice = Choices.Paper;
+
+            //act
+            var actual = Play(userChoice, aiChoice);
+            var expected = "Draw";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Play_GivenPlayerHasRock_AndOpponentHasRock_ShouldReturnDraw()
+        {
+            //Arrange
+            var userChoice = Choices.Rock;
+            var aiChoice = Choices.Rock;
+
+            //act
+            var actual = Play(userChoice, aiChoice);
+            var expected = "Draw";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Play_GivenPlayerHasRock_AndOpponentHasScissors_ShouldReturnPlayerWins()
+        {
+            //Arrange
+            var userChoice = Choices.Rock;
+            var aiChoice = Choices.Scissors;
+
+            //act
+            var actual = Play(userChoice, aiChoice);
+            var expected = "Wins";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void Play_GivenPlayerHasRock_AndOpponentHasPaper_ShouldReturnPlayerLoses()
+        {
+            //Arrange
+            var userChoice = Choices.Rock;
+            var aiChoice = Choices.Paper;
+
+            //act
+            var actual = Play(userChoice, aiChoice);
+            var expected = "Loss";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Play_GivenPlayerHasScissor_AndOpponentHasRock_ShouldReturnPlayerLoses()
+        {
+            //Arrange
+            var userChoice = Choices.Scissors;
+            var aiChoice = Choices.Rock;
+
+            //act
+            var actual = Play(userChoice, aiChoice);
+            var expected = "Loss";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Play_GivenPlayerHasScissors_AndOpponentHasPaper_ShouldReturnPlayerWins()
+        {
+            //Arrange
+            var userChoice = Choices.Scissors;
+            var aiChoice = Choices.Paper;
+
+            //act
+            var actual = Play(userChoice, aiChoice);
+            var expected = "Wins";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Play_GivenPlayerHasScissors_AndOpponentHasScissors_ShouldReturnDraw()
+        {
+            //Arrange
+            var userChoice = Choices.Scissors;
+            var aiChoice = Choices.Scissors;
+
+            //act
+            var actual = Play(userChoice, aiChoice);
+            var expected = "Draw";
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private string Play(Choices userChoice, Choices aiChoice)
+        {
+            if (aiChoice == userChoice)
+                return "Draw";
+
+            switch (userChoice)
+            {
+                case Choices.Paper:
+                    if (aiChoice == Choices.Rock)
+                        return "Wins";
+                    return "Loss";
+                case Choices.Rock:
+                    if (aiChoice == Choices.Paper)
+                        return "Loss";
+                   return "Wins";
+                case Choices.Scissors:
+                    if (aiChoice == Choices.Paper)
+                        return "Wins";
+                    return "Loss";
+            }
+            return "";
+        }
+    }
+
+    enum Choices
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+}
